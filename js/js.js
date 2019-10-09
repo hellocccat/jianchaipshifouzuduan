fa57a457e2cbd3f64af9a66e90abf9ec9656c1ee
kieng1997 <admin@kieng.cn>
2019-10-09 09:33:23 +0800

    错误提示

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 //隐藏进度条先
-	$('.mdui-btn').click(function(){
-		$('.mdui-list').show();
-		h();
+$('.mdui-btn').click(function(){
+	$('.mdui-list').show();
+	h();
 		// 禁用按钮
 		$('.mdui-btn').attr('disabled','disabled');
 		var ip = $('#ip').val();
@@ -12,42 +12,51 @@
 		// 国内
 		$.post(cnurl,{'ip':ip,'port':port},function(data){
 			cn();
-			$('#cntcp').html(data.tcp);
-			$('#cnicmp').html(data.icmp);
-			$(".mdui-btn").removeAttr("disabled");
+			if (data.code==1) {
+				mdui.alert(data.msg)
+			}else{
+				$('#cntcp').html(data.tcp);
+				$('#cnicmp').html(data.icmp);
+				$(".mdui-btn").removeAttr("disabled");
+			}
+			
 		});
 		// 国外
 		$.post(enurl,{'ip':ip,'port':port},function(data){
 			en();
-			$('#entcp').html(data.tcp);
-			$('#enicmp').html(data.icmp);
-			$(".mdui-btn").removeAttr("disabled");
+			if (data.code==1) {
+				mdui.alert(data.msg)
+			}else{
+				$('#entcp').html(data.tcp);
+				$('#enicmp').html(data.icmp);
+				$(".mdui-btn").removeAttr("disabled");
+			}
 		});
 		return false;
 	});
 
-	function h(){
-		$('#cntcpj').show();
-		$('#cnicmpj').show();
-		$('#cntcp').hide();
-		$('#cnicmp').hide();
-		$('#entcpj').show();
-		$('#enicmpj').show();
-		$('#entcp').hide();
-		$('#enicmp').hide();
-	}
+function h(){
+	$('#cntcpj').show();
+	$('#cnicmpj').show();
+	$('#cntcp').hide();
+	$('#cnicmp').hide();
+	$('#entcpj').show();
+	$('#enicmpj').show();
+	$('#entcp').hide();
+	$('#enicmp').hide();
+}
 
-	function cn(){
-		$('.mdui-list').show();
-		$('#cntcpj').hide();
-		$('#cnicmpj').hide();
-		$('#cntcp').show();
-		$('#cnicmp').show();
-	}
-	function en(){
-		$('.mdui-list').show();
-		$('#entcpj').hide();
-		$('#enicmpj').hide();
-		$('#entcp').show();
-		$('#enicmp').show();
-	}
+function cn(){
+	$('.mdui-list').show();
+	$('#cntcpj').hide();
+	$('#cnicmpj').hide();
+	$('#cntcp').show();
+	$('#cnicmp').show();
+}
+function en(){
+	$('.mdui-list').show();
+	$('#entcpj').hide();
+	$('#enicmpj').hide();
+	$('#entcp').show();
+	$('#enicmp').show();
+}
